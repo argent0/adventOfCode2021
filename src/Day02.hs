06f090e6bff026c8f1bccc2171e9606407d381ae
Aner Lucero <4rgento@gmail.com>
2021-12-13 23:07:27 -0300

    Day 02, both parts

--- src/Day02.hs
+++ src/Day02.hs
@@ -1,27 +1,56 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Day02
     ( runSolution
     ) where
 
 import qualified Data.ByteString as BS
 import qualified Data.List as DL
-import qualified Control.Applicative as CA
 
-type Input = [Int]
+import qualified Data.Attoparsec.ByteString as DAB
+import qualified Data.Attoparsec.ByteString.Char8 as DABC8
+
+import qualified Linear as L
+import Control.Lens
+
+data Command
+	= Forward Int
+	| Down Int
+	| Up Int
+	deriving Show
+
+type Input = [Command]
 type Result = Int
 
--- Solution: 1655
+-- Solution: 2073315
 solver1 :: Input -> Result
-solver1 = undefined
+solver1 = (\(L.V2 x y) -> x * y) . DL.foldl' (+) (L.V2 0 0) . (mapper <$>)
+	where
+	mapper (Forward x) = L.V2 x 0
+	mapper (Down x) = L.V2 0 x
+	mapper (Up x) = L.V2 0 (-x)
 
--- Solution: 1683
+-- Solution: 1840311528
 solver2 :: Input -> Result
-solver2 = undefined
+solver2 = (\(L.V2 x y) -> x * y) . fst . DL.foldl' folder (L.V2 0 0, 0)
+	where
+	folder :: (L.V2 Int, Int) -> Command -> (L.V2 Int, Int)
+	folder (L.V2 px py, aim) (Forward x) = (L.V2 (px + x) (py + aim * x), aim)
+	folder (pos, aim) (Down x) = (pos, aim + x)
+	folder (pos, aim) (Up x) = (pos, aim - x)
 
-simpleParser :: String -> Input
-simpleParser = undefined
+simpleParser :: DAB.Parser Input
+simpleParser = DAB.sepBy'
+	( DAB.choice
+		[ DAB.try $ DAB.string "forward " *> (Forward <$> DABC8.decimal)
+		, DAB.try $ DAB.string "down " *> (Down <$> DABC8.decimal)
+		, DAB.try $ DAB.string "up " *> (Up <$> DABC8.decimal) ]
+	) (DABC8.char '\n')
 
+-- Higher than 1005
 runSolution :: FilePath -> IO ()
 runSolution inputPath = do
-	contents <- readFile inputPath
-	let input = simpleParser contents
-	print $ solver1 input
+	contents <- BS.readFile inputPath
+	let parseResult = DAB.parseOnly simpleParser contents
+	case parseResult of
+		Left error -> putStrLn error
+		Right input -> print $ solver2 input
