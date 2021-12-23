372342c7abc8e2ad259d0b70684447f83e568263
Aner Lucero <4rgento@gmail.com>
2021-12-23 17:37:38 -0300

    Day 03 first part

--- src/Day03.hs
+++ src/Day03.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module Day03
     ( runSolution
     ) where
@@ -5,23 +6,57 @@ module Day03
 import qualified Data.ByteString as BS
 import qualified Data.List as DL
 import qualified Control.Applicative as CA
+import Data.Functor (($>))
+import qualified Data.Bits as DB
 
-type Input = [Int]
+import qualified Data.Attoparsec.ByteString as DAB
+import qualified Data.Attoparsec.ByteString.Char8 as DABC8
+
+data Bit = Zero | One deriving (Show, Eq, Ord, Enum)
+type Bits = [Bit]
+type Input = [Bits]
 type Result = Int
 
--- Solution: 1655
+gammaRate :: Int -> Input -> Int
+gammaRate size = snd . DL.foldl' folder' (0 :: Int, DB.zeroBits :: Int) . reverse . fmap mapper . DL.foldl' folder (replicate size (0, 0))
+	where
+	folder' :: (Int, Int) -> Bit -> (Int, Int)
+	folder' (l, g) Zero = (l + 1, DB.clearBit g l)
+	folder' (l, g) One = (l + 1, DB.setBit g l)
+	mapper :: (Int, Int) -> Bit
+	mapper (zeros, ones)
+		| zeros > ones = Zero
+		| otherwise = One
+	folder :: [(Int, Int)] -> Bits -> [(Int, Int)]
+	folder acc bits = zipWith zipper acc bits
+	zipper :: (Int, Int) -> Bit -> (Int, Int)
+	zipper (zeros, ones) Zero = (zeros + 1, ones)
+	zipper (zeros, ones) One = (zeros, ones + 1)
+
+-- Solution: 
 solver1 :: Input -> Result
-solver1 = undefined
+solver1 hs = undefined
 
--- Solution: 1683
+-- Solution: 
 solver2 :: Input -> Result
-solver2 = undefined
+solver2 hs = undefined
 
-simpleParser :: String -> Input
-simpleParser = undefined
+simpleParser :: DAB.Parser Input
+simpleParser = DAB.sepBy'
+	(DAB.many1' $ DAB.choice
+		[ DAB.try $ DABC8.char '0' $> Zero
+		, DAB.try $ DABC8.char '1' $> One ] )
+	(DABC8.char '\n')
 
 runSolution :: FilePath -> IO ()
 runSolution inputPath = do
-	contents <- readFile inputPath
-	let input = simpleParser contents
-	print $ solver1 input
+	contents <- BS.readFile inputPath
+	let parseResult = DAB.parseOnly simpleParser contents
+	case parseResult of
+		Left error -> putStrLn error
+		Right (h:t) -> do
+			let gr = gammaRate (length h) (h:t)
+			let er = DB.complement gr `DB.xor` DB.shiftL (DB.complement (DB.zeroBits :: Int)) (length h)
+			print (gr, er)
+			print $ gr * er
+		Right _ -> putStrLn "Error"
